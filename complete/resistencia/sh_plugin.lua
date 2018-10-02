78a1126d57ffeae182062a7d4932b7fe0ebb6c73
jopnine <ddeviljo@gmail.com>
2018-10-02 13:47:28 -0300

    Add files via upload

---
+++ complete/resistencia/sh_plugin.lua
@@ -0,0 +1,12 @@
+local PLUGIN = PLUGIN
+PLUGIN.name = "Resistencia"
+PLUGIN.author = "B4tr1p"
+PLUGIN.desc = "It will be adding some damagescale on item users/armors"
+--- in progress
+
+hook.add("entitytaked, {
+
+
+
+
+})
